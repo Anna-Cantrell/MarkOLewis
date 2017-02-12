a07576afe7aab55e4af391203bcda58013563b91
Anna Cantrell <cantrelldm@appstate.edu>
2017-02-12 17:42:51 -0500

    Update header.php

--- header.php
+++ header.php
@@ -15,11 +15,16 @@
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+      
+      <!--[if IE 8]>
+      <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="   crossorigin="anonymous"></script>
+<link href="<?php bloginfo('template_directory');?>/dev8.css" rel="stylesheet" type="text/css" media="screen, projection">
+     <![endif]-->
    
     <!--- this is the "wrong" method to insert css. css inserted through functions theme. leaving this here for back up though.
 
